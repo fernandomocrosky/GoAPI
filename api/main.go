a6cbf6465cffa11feadae0e0d497684e238b4da0
Fernando Mocrosky <fernandobismaia@gmail.com>
2024-06-27 23:19:09 -0300

    Remove go.mod warnings and constring print

--- api/main.go
+++ api/main.go
@@ -11,7 +11,6 @@ import (
 func main() {
 	config.Load()
 	r := router.Make()
-	fmt.Println(config.ConnString)
 
 	fmt.Printf("Server started on port %d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
